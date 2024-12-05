2e8aaec177f02650bc9efbbed404a338918d953b
Nau-stack-110 <andriatahianaarnaud110@gmail.com>
2024-12-05 13:29:14 +0300

    d

--- config/config.js
+++ config/config.js
@@ -15,10 +15,10 @@ module.exports = {
       "dialect": "mysql"
     },
     "production": {
-      "username": "udgkuwo0xrrzc7nm",
-      "password": "udgkuwo0xrrzc7nm",
-      "database": "bc7ryr9lhasequprjfua",
-      "host": "bc7ryr9lhasequprjfua-mysql.services.clever-cloud.com",
+      "username":process.env.DB_USERNAME,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_DATABASE,
+      "host": process.env.DB_HOST,
       "dialect": "mysql"
     }
 }
