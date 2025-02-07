71f6a5f81ca5050eb43a50f173aa74784b6c19c7
Nau-stack-110 <andriatahianaarnaud110@gmail.com>
2025-02-08 02:30:29 +0300

    maj 11

--- routers/trajet.js
+++ routers/trajet.js
@@ -4,9 +4,10 @@ const trajetController = require('../controllers/trajet.controller');
 const { CheckToken, CheckAdmin } = require('../middlewares/authorize');
 
 router.post('/', [CheckToken, CheckAdmin],  trajetController.createTrajet );
+router.post('/betsaka', [CheckToken, CheckAdmin],  trajetController.createTrajetPls );
+
 router.put('/:id', [CheckToken, CheckAdmin],  trajetController.updateTrajet );
 router.delete('/:id', [CheckToken, CheckAdmin], trajetController.deleteTrajet );
-
 
 router.get('/', trajetController.getAllTrajet );
 router.get('/available-taxibe', trajetController.getAvailableTaxi );
